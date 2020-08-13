4b651605b5080c05e1f4a97c29f62ac9af30c97e
Amy <chengamy5@gmail.com>
2020-08-13 16:45:06 -0700

    Initial Commit

---
+++ Schools Connect App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schools Connect App
+//
+//  Created by Apple on 8/13/20.
+//  Copyright © 2020 Amy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Schools Connect AppTests/Schools_Connect_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Schools_Connect_AppTests.swift
+//  Schools Connect AppTests
+//
+//  Created by Apple on 8/13/20.
+//  Copyright © 2020 Amy. All rights reserved.
+//
+
+import XCTest
+@testable import Schools_Connect_App
+
+class Schools_Connect_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
